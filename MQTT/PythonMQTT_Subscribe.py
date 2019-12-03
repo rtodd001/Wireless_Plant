1b6d863ca714dc460c5bf01f9ebff81e8954c878
rtodd001 <rtodd001@ucr.edu>
2019-12-03 20:35:37 +0000

    Code should work but the microphone config is broken

--- MQTT/PythonMQTT_Subscribe.py
+++ MQTT/PythonMQTT_Subscribe.py
@@ -4,6 +4,7 @@ Thomas Varnish (https://github.com/tvarnish), (https://www.instructables.com/mem
 Written for my Instructable - "How to use MQTT with the Raspberry Pi and ESP8266"
 """
 import paho.mqtt.client as mqtt
+from mic_rec import SpeechText
 
 # Don't forget to change the variables for the MQTT broker!
 mqtt_username = "rtodd"
@@ -19,7 +20,7 @@ client.username_pw_set(mqtt_username, mqtt_password)
 # to the broker, and what happens then the topic receives a message
 def on_connect(client, userdata, flags, rc):
     # rc is the error code returned when connecting to the broker
-    print "Connected!", str(rc)
+    print ("Connected!", str(rc))
     
     # Once the client has connected to the broker, subscribe to the topic
     client.subscribe(mqtt_topic)
@@ -29,8 +30,18 @@ def on_message(client, userdata, msg):
     # If you want to check each message, and do something depending on
     # the content, the code to do this should be run in this function
     
-    print "Topic: ", msg.topic + "\nMessage: " + str(msg.payload)
+    #print "Topic: ", msg.topic + "\nMessage: " + str(msg.payload)
     
+    value = SpeechText()
+
+    if (value.find("status") > -1):
+        if(msg.topic > 150):
+            print ("Plant has been watered")
+        else:
+            print ("Please water the plant")
+    else:
+        print ("Invalid respone")
+
     # The message itself is stored in the msg variable
     # and details about who sent it are stored in userdata
 
